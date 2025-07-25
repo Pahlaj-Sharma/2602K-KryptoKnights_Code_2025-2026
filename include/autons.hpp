94403181dc1c1de1446ce778d8e27fa4f806bf23
Pahlaj_Sharma <pahlaj1219@gmail.com>
2025-07-24 22:42:45 -0400

    fixed prototype

--- include/autons.hpp
+++ include/autons.hpp
@@ -18,5 +18,5 @@ void auton10();
 
 void moveLinear(double inches, int timeout = 2000, float maxspeed = 70, float minspeed = 40);
 void chassisPID(std::string premade = "normal", double lat_kp = chassis.lateralPID.kP, double lat_ki = chassis.lateralPID.kI, double lat_kd = chassis.lateralPID.kD, double lat_slew = chassis.lateralPID.windupRange, double ang_kp = chassis.angularPID.kP, double ang_ki = chassis.angularPID.kI, double ang_kd = chassis.angularPID.kD);
-//void resetOdometry(pros::Distance sensor1, std::string axis);
+void resetOdometry(pros::Distance sensor1, std::string axis, double dist1_center);
 #endif
