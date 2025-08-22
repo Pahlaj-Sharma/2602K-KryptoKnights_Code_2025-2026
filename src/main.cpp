4e76c98f923a7ad907caef985ff19c12a9039ec3
Pahlaj_Sharma <pahlaj1219@gmail.com>
2025-08-22 17:06:08 -0400

    V 3.20 added feedforward

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
  * Project: 2602K-RobotCode
  * Author: Pahlaj Sharma
  * Date Created: June 14, 2025
- * Current Version: 3.10
+ * Current Version: 3.20
  * Last Updated: Aug 7, 2025
  *
  * Copyright (c) 2025, Pahlaj Sharma.
